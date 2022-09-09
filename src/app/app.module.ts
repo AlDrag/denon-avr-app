ceb25025ce5563e59192c3b7d6b09637c68af98b
Alan Dragicevich <aldragicevich@gmail.com>
2022-09-10 11:00:49 +1200

    Replaced Ionic Capacitor http with Cordova http
    Capacitor HTTP doesn't support raw HTTP payloads, which I need for XML payloads to the Denon API.
    
    Cordova HTTP is old and doesn't have a nice interface with Angular unfortunately, but it supports raw HTTP payloads.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    HTTP,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
